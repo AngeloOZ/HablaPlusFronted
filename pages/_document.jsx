943f10df34ca4bcab6f1077dcbc27222c305b07e
Angello Ordoñez <angelo-mjz7@hotmail.com>
2022-11-01 14:12:14 -0500

    font gotham

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -15,7 +15,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-      <link rel="https://cdn.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css" />
+        <link 
+          rel="stylesheet"
+          href="https://cdn.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css"
+        />
 //           <link
 //             href="http://fonts.cdnfonts.com/css/gotham?styles=17581,17583,17587,17588,17589,17591,17592,17595,17590,17594"
 //             rel="stylesheet"
