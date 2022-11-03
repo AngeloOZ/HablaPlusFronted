24e70b0681230be6814cab3c6c42fb139612ed22
AngeloOz <angelo-mjz@hotmail.com>
2022-11-02 20:39:13 -0500

    update document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -19,10 +19,10 @@ class MyDocument extends Document {
             href="https://cdn.jsdelivr.net/gh/AngeloOZ/HablaPlusFronted@main/public/css/font-gotham.css?token=GHSAT0AAAAAAB2VCRMGXDZXZBMIE3GQOVV6Y3BOQDQ"
             rel="stylesheet"
           />
-//                 <link
-//             href="http://fonts.cdnfonts.com/css/gotham?styles=17581,17583,17587,17588,17589,17591,17592,17595,17590,17594"
-//             rel="stylesheet"
-//           />
+                {/* <link
+             href="http://fonts.cdnfonts.com/css/gotham?styles=17581,17583,17587,17588,17589,17591,17592,17595,17590,17594"
+             rel="stylesheet"
+           /> */}
         </Head>
         <body>
           <Main />
